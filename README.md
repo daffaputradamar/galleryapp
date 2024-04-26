3bf59d01623380855f2eefd65947b7f1af698492
Daffa Akbar <daffaputradamar@gmail.com>
2024-04-26 14:45:16 +0700

    Added uploadthing

--- README.md
+++ README.md
@@ -10,6 +10,7 @@
 - [x] Add auth (w/ clerk)
 - [ ] Add image upload
 - [ ] "taint" (server-only)
+- [ ] use Next/Image component
 - [ ] Error management (w/ Sentry)
 - [ ] Routing / image page (parallel route)
 - [ ] Delete button (w/ Server Actions)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ async function Images() {
   return (
   <div className="flex flex-wrap gap-4"> 
       {
-        [...images, ...images, ...images].map((image, idx) => (
-        <div key={idx} className="w-48">
+        [...images].map((image) => (
+        <div key={image.id} className="w-48">
           <img src={image.url} />
           <div>{image.name}</div>
         </div>
